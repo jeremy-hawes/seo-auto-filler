adbe5864cf2e4704fdb0194f56058cec52df76e1
Jeremy Hawes <jeremyhawes@Jeremys-Mac-Pro.local>
2018-05-02 11:09:56 -0700

    force https for images and add target _blank to specified anchor tags

--- seo-auto-filler.js
+++ seo-auto-filler.js
@@ -2,23 +2,37 @@
 Author: Jeremy Hawes
 Title: SEO Auto-Filler
 Description: Automatic Fixes for Simple SEO Best Practices
-Version: 1.0.0
+Version: 1.0.2
 */
 
-$(document).ready(function(){
-    var defaultAlt = 'Default Alt Tag';
-    
-    // Adds Default Alt Tag to All Images Missing One
-    $('img').each(function(){
-        if($(this).attr('alt') == '' || ! $(this).attr('alt')) {
-            $(this).attr('alt', defaultAlt);
-        }
-    });
+jQuery(function($) {
+	$(document).ready(function() {
+		var urlKeyword = "https://codepen.io",
+				defaultAlt = "Default Alt Tag";
 
-    // Adds additional attributes to anchor tags with target="_blank"
-    $('a').each(function(){
-        if($(this).attr('target')) {
-            $(this).attr('rel','noopener noreferrer');
-        }
-    });
+		// Adds Default Alt Tag to All Images Missing One
+		$("img").each(function() {
+			if ($(this).attr("alt") == "" || !$(this).attr("alt")) {
+				$(this).attr("alt", defaultAlt);
+			}
+		});
+		// Adds target _blank to all external links (missing chosen keyword)
+		$('a').each(function() {
+			$(this).not('[href*="' + urlKeyword + '"]').attr('target','_blank');
+		});
+		// Adds additional attributes to anchor tags with target="_blank"
+		$("a").each(function() {
+			if ($(this).attr("target")) {
+				$(this).attr("rel", "noopener noreferrer");
+			}
+		});
+		// Force images with http to use https
+		$('img[href^="http:"]').each(function(){
+			var i = $(this).attr("src");
+			var n = i.replace("http://", "https://");
+			$(this).attr("src", function() {
+				return n
+			});
+		});
+	});
 });
